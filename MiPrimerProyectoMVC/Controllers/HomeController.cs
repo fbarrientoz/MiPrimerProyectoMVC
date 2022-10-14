4f3caaa40e445567d83bf80306beb70a63d3c3f1
Fabiola <Fabiola@LAPTOP-5NFTL38M>
2022-10-13 19:29:01 -0700

    Add project files.

---
+++ MiPrimerProyectoMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace MiPrimerProyectoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //localhost:4876/Home/Index
+        public ActionResult Index()
+        {
+            ViewBag.Algo = "Soy algo";
+            ViewBag.Alumnos = Alumno.Listar();
+            return View();
+        }
+        public ActionResult Ver()
+        {
+            return View();
+        }
+
+        public ActionResult Guardar()
+        {
+            return RedirectToAction("Index");
+        }
+
+        //public ActionResult Alumno()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Profesores()
+        {
+            return View();
+        }
+    }
+}
---
+++ Model/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Alumno
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+
+        public static List<Alumno> Listar()
+        {
+            var alumnos = new List<Alumno>();
+            for(var i = 0; i < 10; i++)
+            {
+                alumnos.Add(new Alumno()
+                {
+                    id = i,
+                    nombre = "Alumno " + i
+                });
+            }
+            return alumnos;
+        }
+
+    }
+}
